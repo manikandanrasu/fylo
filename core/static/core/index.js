eda9ac887c55d39dd1538b913799e205b1495949
manikandanrasu <manikandanrasubj@gmail.com>
2025-07-21 12:29:27 +0530

    Initial commit

---
+++ core/static/core/index.js
@@ -0,0 +1,8 @@
+// Show or hide navbar on click
+
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
