16a0b3f66196c99b27146de8cad5e81252a8a54a
manikandanrasu <manikandanrasubj@gmail.com>
2025-07-30 12:49:18 +0530

    setup postgres

--- fylo/settings.py
+++ fylo/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
-# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,8 +88,12 @@ WSGI_APPLICATION = 'fylo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'docify',
+        'USER': 'postgres',
+        'PASSWORD': '.manikandan@2002psql',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
